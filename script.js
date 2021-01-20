7fa1c736e9d44432dddd634c2db974542cbaa9ba
banuakman <banu@asterya.com>
2021-01-19 19:23:52 -0500

    added  pseudocode

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create a timer attached to a button with a starting value of 75
+// When timer is pressed start a reverse countdown
+
+// When countdown starts, start quiz
+// Create the quiz object: question, choices, answer
+// When user selects the right answer, textcontent "It's Correct!"
+// When user selects the wrong answer, textcontent "It's Wrong!"
+// When user selects the wrong answer, 5 seconds will be deducted from the countdown
+
+// Final score will keep track of correct answers
